63202123928a5b8b6a8c6a2200cfb36fefd2ce34
Kristian Mandrup <kmandrup@gmail.com>
2020-11-19 10:26:31 +0100

    Feeder component added to main page - linked to Feeder project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Feedback from "feeder-react-feedback"; // import Feedback component
+import "feeder-react-feedback/dist/feeder-react-feedback.css"; // import stylesheet
 
 function App() {
   return (
     <div className="App">
+      <Feedback projectId="5fb639e010b31800049eaddf" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
